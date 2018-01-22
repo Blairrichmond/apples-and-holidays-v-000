ca4b5b3e54004b73b06df30c265f5eca793eadd1
Brent  Ransom <sadumedusa@gmail.com>
2018-01-22 21:44:30 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,7 +69,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_name = []
-  holiday_hash each do |season, holiday|
+  holiday_hash.each do |season, holiday|
     holiday.each do |supply|
       if supply == "BBQ"
         holiday_name << season
