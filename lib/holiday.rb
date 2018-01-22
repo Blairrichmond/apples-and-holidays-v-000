3467dd0752e48733b62b08a479635b6560b25883
Brent  Ransom <sadumedusa@gmail.com>
2018-01-22 20:15:04 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -28,7 +28,6 @@ end
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   holiday_hash[:spring][:memorial_day] << "Grill"
-  holiday_hash[:winter][:memorial_day] << "Drinks"
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
