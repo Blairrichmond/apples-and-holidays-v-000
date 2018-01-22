93596cceea20d7f58b453d6f8361a4a217b2bad9
Brent  Ransom <sadumedusa@gmail.com>
2018-01-22 19:56:45 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -17,7 +17,12 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #     :memorial_day => ["BBQ"]
   #   }
   # }
-  # return the second element in the 4th of July array
+  second_supply_for_fourth_of_july.each do |season, holiday|
+    if holiday == :memorial_day
+      holiday.each do |event|
+        puts "#{event}"
+      end
+    end
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
