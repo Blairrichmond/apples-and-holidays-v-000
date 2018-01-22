94158fb77113860d84423d75bd9fa140c641dd8a
Brent  Ransom <sadumedusa@gmail.com>
2018-01-22 21:37:15 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -53,7 +53,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
 
   holiday_hash.each do |season, holiday, item|
-    puts "#{season} #{holiday} #{item}"
+    puts "#{season}" "#{holiday}" "#{item}"
   end
 end
 
